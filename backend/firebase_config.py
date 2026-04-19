a1c807811e79a08a0c04fab228db60855383340f
priyamredker <redkerpriyam12@gmail.com>
2026-04-19 14:14:50 +0530

    Added Firestore integration and backend history APIs

---
+++ backend/firebase_config.py
@@ -0,0 +1,11 @@
+import os
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+key_path = os.path.join(BASE_DIR, "data", "firebase_key.json")
+
+cred = credentials.Certificate(key_path)
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
